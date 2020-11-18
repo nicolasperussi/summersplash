9167466d30c3bdfbe074e172a6dbf34dab21e3db
Nicolas Perussi <nicolasperussi12@hotmail.com>
2020-11-18 17:30:53 -0300

    added responsivity to first section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById('mySidenav').style.width = '250px'
+    document.getElementById('mySidenav').style.boxShadow = '-5px 0px 600px 0px #000'
+}
+
+function closeNav() {
+    document.getElementById('mySidenav').style.width = '0'
+    document.getElementById('mySidenav').style.boxShadow = ''
+}
